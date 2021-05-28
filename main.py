e9d7640bd1352620b7a5c9087e5f1987d68e0174
d-stoll <danielsimon.stoll2@gmail.com>
2021-05-28 19:50:14 +0200

    Cleanup

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ async def _price(ctx, arg):
         ticker = yf.Ticker(arg)
         current = ticker.info['regularMarketPrice']
         previous = ticker.info['previousClose']
+        long_name = ticker.info['longName']
+        symbol = ticker.info['symbol']
         change = ((current - previous) / previous) * 100
-        msg = (f"The market price of **{ticker.info['longName']} ({ticker.info['symbol']})** is "
-               f"**{round(current, 2)}$** ({'{0:+}'.format(round(change, 2))}%)"
-               "  <:stonks:785565572300800050>")
+        emoji = "<:stonks:785565572300800050>" if change >= 0 else "<:stonks:785565572300800050>"
+        msg = (f"The market price of **{long_name} ({symbol})** is **{round(current, 2)}$** "
+               f"({'{0:+.2f}'.format(change)}%)  {emoji}")
         await ctx.send(msg)
     except:
         await ctx.send(f"{arg.upper()} gibts ned oida! <:ThomasPassAuf:788838985878994964>")
 
 
 bot.run(os.environ['DISCORD_TOKEN'])
-
-# test
