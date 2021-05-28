2cecf3f7ad47c829781189aac2789bcde80f6c37
d-stoll <danielsimon.stoll2@gmail.com>
2021-05-28 18:35:45 +0200

    Added change of stock in discord message

--- main.py
+++ main.py
@@ -9,9 +9,16 @@ bot = commands.Bot(command_prefix='$')
 async def _price(ctx, arg):
     try:
         ticker = yf.Ticker(arg)
-        await ctx.send(
-            f"The market price of **{ticker.info['longName']} ({ticker.info['symbol']})** is **{round(ticker.info['regularMarketPrice'], 2)}$**  <:stonks:785565572300800050>")
+        current = ticker.info['regularMarketPrice']
+        previous = ticker.info['previousClose']
+        change = ((current - previous) / previous) * 100
+        sign = "+" if change >= 0 else ""
+        msg = (f"The market price of **{ticker.info['longName']} ({ticker.info['symbol']})** is "
+               f"**{round(current, 2)}$** ({sign}{round(change, 2)}%)"
+               "  <:stonks:785565572300800050>")
+        await ctx.send(msg)
     except:
         await ctx.send(f"{arg.upper()} gibts ned oida?! <:ThomasPassAuf:788838985878994964>")
+
 
 bot.run(os.environ['DISCORD_TOKEN'])
