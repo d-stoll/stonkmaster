1631dc7e3a1f8802278eb70a09f67ac8f1a42873
Kili <kilian.warmuth@tum.de>
2021-05-29 13:42:55 +0200

    updated stock chart

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ async def _shorts(ctx, arg):
                                                      low=stocks[('Low', arg)],
                                                      close=stocks[('Close', arg)])])
 
-        chart_title = f"Stock chart of {arg}"
+        chart_title = f"Chart of **{arg}**"
         candlestick.update_layout(xaxis_rangeslider_visible=False, Title=chart_title)
         candlestick.update_yaxes(tickprefix='$')
 
