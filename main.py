e866759bd556681746cb7fca1b0277fbc590f445
Kili <kilian.warmuth@tum.de>
2021-05-29 12:19:09 +0200

    print stock chart

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import yfinance as yf
 from discord.ext import commands
 import os
+import datetime as dt
+import pandas_datareader.data as web
+import plotly.graph_objects as go
 
 bot = commands.Bot(command_prefix='$')
 
@@ -31,8 +34,32 @@ async def _shorts(ctx, arg):
         shares_short = ticker.info['sharesShort']
         short_percent_of_float = ticker.info['shortPercentOfFloat'] * 100
         msg = (f"Currently **{'{:,}'.format(shares_short)} shares** of **{long_name} ({symbol})** are shorted. "
-               f"This corresponds to **{round(short_percent_of_float, 2)}%** of shares available. Real SI may be much higher. Hedgies are fucked.")
+               f"This corresponds to **{round(short_percent_of_float, 2)}%** of shares available.")
         await ctx.send(msg)
+        await ctx.send("Real SI may be much higher -> Hedgies are fucked.")
+    except:
+        await ctx.send(f"{arg.upper()} gibts ned oida! <:ThomasPassAuf:788838985878994964>")
+
+@bot.command(name='chart')
+async def _shorts(ctx, arg):
+    try:
+        start = dt.datetime(2021, 1, 1)
+        end = dt.datetime.now()
+
+        stocks = web.DataReader([arg], 'yahoo', start, end)
+        # stocks_close = pd.DataFrame(
+        #  web.DataReader(['GME'], 'yahoo', start, end)['Close'])
+        candlestick = go.Figure(data=[go.Candlestick(x=stocks.index,
+                                                     open=stocks[('Open', arg)],
+                                                     high=stocks[('High', arg)],
+                                                     low=stocks[('Low', arg)],
+                                                     close=stocks[('Close', arg)])])
+
+        candlestick.update_layout(xaxis_rangeslider_visible=False, title='stock chart')
+        candlestick.update_xaxes(title_text='Date')
+        candlestick.update_yaxes(title_text='Close Price', tickprefix='$')
+
+        candlestick.write_image("/home/kili/stock.png")
     except:
         await ctx.send(f"{arg.upper()} gibts ned oida! <:ThomasPassAuf:788838985878994964>")
 
