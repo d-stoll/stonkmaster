68b15314c12cb09b14f601f8c00a185c6c76d1dd
d-stoll <danielsimon.stoll2@gmail.com>
2021-06-06 21:57:31 +0200

    Added descriptions to commands

--- stonkmaster/main.py
+++ stonkmaster/main.py
@@ -6,26 +6,38 @@ from stonkmaster.commands.ChartCommand import ChartCommand
 from stonkmaster.commands.PriceCommand import PriceCommand
 from stonkmaster.commands.ShortsCommand import ShortsCommand
 
-bot = commands.Bot(command_prefix='$')
+help_command = commands.DefaultHelpCommand(
+    no_category="Commands"
+)
+
+description = ("The Stonk Master is a Discord bot for fellow apes to monitor stonks without "
+               "leaving their gaming habitat. It presents information about stonks in a very "
+               "easy and simple way."
+               )
+
+bot = commands.Bot(command_prefix='$', description= description, help_command=help_command)
 
 
-@bot.command(name='price')
+@bot.command(name="price",
+             description="Shows the current price of the stonk, as well as its daily change.")
 async def _price(ctx, arg):
     await PriceCommand().run(ctx, arg)
 
 
-@bot.command(name='shorts')
+@bot.command(name="shorts",
+             description="Provides currently known information on how heavily the stonk is shorted.")
 async def _shorts(ctx, arg):
     await ShortsCommand().run(ctx, arg)
 
 
-@bot.command(name='chart')
+@bot.command(name="chart",
+             description="Generates a chart showing the price development of the share in the last months.")
 async def _chart(ctx, arg):
     await ChartCommand().run(ctx, arg)
 
 
 def main():
-    bot.run(os.environ['DISCORD_TOKEN'])
+    bot.run(os.environ["DISCORD_TOKEN"])
 
 
 if __name__ == "__main__":
