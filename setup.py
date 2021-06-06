7e58a4f9e33d626c7d1ac1e6f038cb50eaa3342c
d-stoll <danielsimon.stoll2@gmail.com>
2021-06-06 15:39:53 +0200

    Restructured project

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='stonkmaster',
+    version='0.0.1',
+    description='Discord bot for stonks',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    url='https://github.com/d-stoll/stonkmaster',
+    license='Apache License 2.0',
+    keywords='discord discord-bot stocks',
+
+    packages=find_packages(exclude=['tests', 'tests.*']),
+
+    python_requires='>=3.7, <4',
+    install_requires=[
+        'yfinance',
+        'discord.py',
+        'holidays',
+        'plotly',
+        'pandas'
+    ],
+
+    entry_points='''
+        [console_scripts]
+        stonkmaster=stonkmaster.main:main
+    ''',
+)
