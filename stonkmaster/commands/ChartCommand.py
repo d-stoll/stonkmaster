b5375d8df14dc8c95ba056a16abb6b6e96a8d280
Daniel Stoll <danielsimon.stoll2@gmail.com>
2021-06-07 19:54:18 +0200

    Update ChartCommand.py

--- stonkmaster/commands/ChartCommand.py
+++ stonkmaster/commands/ChartCommand.py
@@ -32,7 +32,7 @@ class ChartCommand:
                 picture = discord.File(f)
                 await ctx.send(file=picture)
 
-            await ctx.send(f"Fia an bessern Graph schaust moi do vorbei: <https://finance.yahoo.com/chart/{symbol}/>")
+            await ctx.send(f"Fia an bessern Graph schaust moi do vorbei: <https://finance.yahoo.com/chart/{info.symbol}/>")
 
         except:
             await ctx.send(f"{arg.upper()} gibt's ned oida! {self.emoji_error}")
