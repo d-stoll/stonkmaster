bde892327402a4414ec47918aee1d5715e01c496
d-stoll <danielsimon.stoll2@gmail.com>
2021-06-15 12:02:52 +0200

    Fixed links in SEC command

--- stonkmaster/commands/SecCommand.py
+++ stonkmaster/commands/SecCommand.py
@@ -22,8 +22,9 @@ class SecCommand:
                 color=0x00ff00)
             urls = filings.get_urls()
             for i, url in enumerate(urls[yf_ticker.info['symbol'].lower()][:5]):
-                accession_number = filings.get_accession_number(url)
-                filings_embed.add_field(name=f"{i + 1} - {accession_number}", value=url, inline=False)
+                accession_number = filings.get_accession_number(url).removesuffix(".txt")
+                html_link = url.removesuffix(".txt") + "-index.html"
+                filings_embed.add_field(name=f"{i + 1} - {accession_number}", value=html_link, inline=False)
             await ctx.send(embed=filings_embed)
         except:
             await ctx.send(f"{ticker.upper()} gibt's ned oida! {self.emoji_error}")
