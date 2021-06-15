159c283c4eb27662ec7651603bc4f7ec2b16ab35
d-stoll <danielsimon.stoll2@gmail.com>
2021-06-15 11:53:43 +0200

    Changed the structure of embed in SecCommand

--- stonkmaster/commands/SecCommand.py
+++ stonkmaster/commands/SecCommand.py
@@ -10,15 +10,19 @@ class SecCommand:
 
     async def run(self, ctx, ticker, type):
         try:
-            await ctx.send(f"**Searching EDGAR database... {self.emoji_search}**")
+            await ctx.send(f"**{self.emoji_search} Searching EDGAR database...**")
             yf_ticker = yf.Ticker(ticker)
-            filings = Filing(cik_lookup=yf_ticker.info['symbol'], filing_type=FilingType(type))
+            filings = Filing(cik_lookup=yf_ticker.info['symbol'].lower(), filing_type=FilingType(type))
 
-            filings_embed = discord.Embed(title=f"SEC filings for {yf_ticker.info['longName']}({yf_ticker.info['symbol']})",
-                                      color=0x00ff00)
+            filings_embed = discord.Embed(
+                title=f"Latest SEC filings of {yf_ticker.info['longName']} ({yf_ticker.info['symbol']})",
+                description=(f"List of {type} filings recently submitted by {yf_ticker.info['longName']} to the "
+                             "United States Securities and Exchange Commission"),
+                color=0x00ff00)
             urls = filings.get_urls()
-            for i, url in enumerate(urls[yf_ticker.info['symbol']][:5]):
-                filings_embed.add_field(name=f"{i}:", value=url, inline=False)
+            for i, url in enumerate(urls[yf_ticker.info['symbol'].lower()][:5]):
+                accession_number = filings.get_accession_number(url)
+                filings_embed.add_field(name=f"{i} - {accession_number}", value=url, inline=False)
             await ctx.send(embed=filings_embed)
         except:
             await ctx.send(f"{ticker.upper()} gibt's ned oida! {self.emoji_error}")
