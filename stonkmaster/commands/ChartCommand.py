64abf6e05bd90fe2df3376055c0b1c819cfadac9
d-stoll <danielsimon.stoll2@gmail.com>
2021-09-07 13:22:18 +0200

    Added time range to chart command

--- stonkmaster/commands/ChartCommand.py
+++ stonkmaster/commands/ChartCommand.py
@@ -1,6 +1,7 @@
 import configparser
 import datetime as dt
 import logging
+import re
 
 import discord
 import pandas_datareader.data as web
@@ -17,17 +18,34 @@ class ChartCommand(commands.Cog,
         self.config = config
 
     @commands.command(name="chart")
-    async def _chart(self, ctx, ticker):
+    async def _chart(self, ctx: commands.Context, ticker: str, range: str):
         try:
+            days_pattern = re.compile("[0-9]+d")
+            months_pattern = re.compile("[0-9]+m")
+            years_pattern = re.compile("[0-9]+y")
+
+            if days_pattern.match(range):
+                diff = dt.timedelta(days=int(range.removesuffix('d')))
+            elif months_pattern.match(range):
+                diff = dt.timedelta(days=int(range.removesuffix('m')))
+            elif years_pattern.match(range):
+                diff = dt.timedelta(days=int(range.removesuffix('y')))
+            else:
+                logging.info(f"{ctx.author.display_name} tried to generate graph with invalid range {range}")
+                await ctx.send("The time range must be specified in days (d), months (m) or years (m). " +
+                               "Example: 7d (7 days)")
+                return
+
             yf_ticker = yf.Ticker(ticker)
             info = yf_ticker.info
 
             if len(info) <= 1:
+                logging.info(f"{ctx.author.display_name} tried to generate graph for invalid ticker {ticker}")
                 await ctx.send(f"{ticker.upper()} gibt's ned oida! {self.config['emojis']['NotFound']}")
                 return
 
-            start = dt.datetime(2021, 1, 1)
             end = dt.datetime.now()
+            start = end - diff
 
             stocks = web.DataReader([ticker], 'yahoo', start, end)
             candlestick = go.Figure(data=[go.Candlestick(x=stocks.index,
@@ -40,8 +58,8 @@ class ChartCommand(commands.Cog,
             candlestick.update_layout(xaxis_rangeslider_visible=False, title=chart_title)
             candlestick.update_yaxes(tickprefix='$')
 
-            candlestick.write_image(f"/tmp/{info['symbol']}.png")
-            with open(f"/tmp/{info['symbol']}.png", 'rb') as f:
+            candlestick.write_image(f"{self.config['stonkmaster']['TmpFolder']}/{info['symbol']}.png")
+            with open(f"{self.config['stonkmaster']['TmpFolder']}/{info['symbol']}.png", 'rb') as f:
                 picture = discord.File(f)
                 await ctx.send(file=picture)
 
