d5497c090417bfdfd3ff6e35932003d214c35e7f
Kilian Warmuth <kilian.warmuth@tum.de>
2021-09-13 17:22:06 +0200

    better search results

--- stonkmaster/commands/WikiCommand.py
+++ stonkmaster/commands/WikiCommand.py
@@ -18,21 +18,26 @@ class WikiCommand(commands.Cog,
     async def _wiki(self, ctx, *keywords):
         try:
 
-            term = "".join(keywords)
-            url = f"https://www.investopedia.com/terms/{term[0]}/{term}.asp"
+            term = "+".join(keywords)
+            url = f"https://www.investopedia.com/search?q={term}"
             soup = BeautifulSoup(requests.get(url).text,
                                  "html.parser")
-            required_data = None
-            for item in soup.select("#mntl-sc-page_1-0"):
-                required_data = item.select("p")[0].text.strip()
-
-            if required_data is None:
+            title = soup.find("h3", id="search-results__title_1-0")
+            if title is None:
                 await ctx.send(
                     f"Check no amoi dein Suchterm ob oder schau selber noch du faule Sau: "
                     f"<https://www.investopedia.com/> {self.config['emojis']['NotFound']}")
                 return
 
-            msg = discord.Embed(title=" ".join(keywords).title(), url=url, description=required_data)
+            url = soup.find("div", id="search-results__url_1-0").text.strip()
+            soup = BeautifulSoup(requests.get(url).text,
+                                 "html.parser")
+
+            required_data = None
+            for item in soup.select("#mntl-sc-page_1-0"):
+                required_data = item.select("p")[0].text.strip()
+
+            msg = discord.Embed(title=title.text.strip(), url=url, description=required_data)
             await ctx.send(embed=msg)
 
         except Exception as ex:
