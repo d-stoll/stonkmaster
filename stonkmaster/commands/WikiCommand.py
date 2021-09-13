2378345d625b71970a18df703ed356658c3bfc44
Kilian Warmuth <kilian.warmuth@tum.de>
2021-09-13 16:35:16 +0200

    first try of wiki command

---
+++ stonkmaster/commands/WikiCommand.py
@@ -0,0 +1,34 @@
+import configparser
+import logging
+
+import discord
+import requests
+from bs4 import BeautifulSoup
+from discord.ext import commands
+
+
+class WatchCommand(commands.Cog,
+                   name="Wiki",
+                   description="Searches investopedia for a definition of the keyword."):
+    def __init__(self, bot: commands.Bot, config: configparser.ConfigParser):
+        self.bot = bot
+        self.config = config
+
+    @commands.command(name="wiki")
+    async def _wiki(self, ctx, *keywords):
+        try:
+
+            term = "".join(keywords)
+            url = f"https://www.investopedia.com/terms/{term[0]}/{term}.asp"
+            soup = BeautifulSoup(requests.get(url).text,
+                                 "html.parser")
+            for item in soup.select("#mntl-sc-page_1-0"):
+                required_data = item.select("p")[0].text.strip()
+
+            msg = discord.Embed(title=" ".join(keywords).title(), url=url, description=required_data)
+            #msg.add_field(name="", value=required_data, inline=False)
+            await ctx.send(embed=msg)
+
+        except Exception as ex:
+            logging.exception(f"Exception in WikiCommand: {ex}")
+            await ctx.send(f"Do hod wos ned bassd, I bin raus. {self.config['emojis']['Error']}")
